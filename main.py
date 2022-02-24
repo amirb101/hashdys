7354ae1c77141c615b2b481abc6eb78cc9183767
amirb101 <amirbattye@gmail.com>
2022-02-24 18:49:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 c,p = map(int,input().split())
 contributers  = []
 projects = []
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 class project():
     def _init_(self):
         self.name = ""
@@ -17,7 +13,6 @@ class project():
 for i in range(p):
     projects.append(project())
     current = list(input().split(" "))
-<<<<<<< Updated upstream
     projects[i].name = current[0]
     projects[i].days = current[1]
     projects[i].score =current[2]
@@ -27,16 +22,3 @@ for i in range(p):
         job = list(input().split(" "))
         project[i].jobs[job[0]] = int(job[1])
 
-=======
-    project[i].name = current[0]
-    project[i].days = current[1]
-    project[i].score =current[2]
-    project[i].best = current[3]
-
-for i in range(c):
-    print("hi")
-class Person():
-  name = ""
-  skills = {}
-  
->>>>>>> Stashed changes
