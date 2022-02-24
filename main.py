9f02054fbe9fa7c4ed0c5e59e825c5d620eccfee
LizziePReed <lizzie.preed@gmail.com>
2022-02-24 18:42:55 +0000

    Update main.py
    making projects class and assigning values to it. all kept in projects array.

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 c,p = map(int,input().split())
 contributers  = []
+projects = []
 class project():
     def _init_(self):
         self.name = ""
         self.days = 0
         self.score = 0
         self.best = 0
-        self.levels = {}
+        self.jobs= {}
+
 
 for i in range(p):
+    projects.append(project())
     current = list(input().split(" "))
-    current
+    project[i].name = current[0]
+    project[i].days = current[1]
+    project[i].score =current[2]
+    project[i].best = current[3]
 
 
 
 
-<<<<<<< HEAD
-    
-=======
+
 for i in range(c):
     print("hi")
 class Person():
   name = ""
   skills = {}
   
->>>>>>> e455676da8fd59604ecbce610bc346d9488198f4
