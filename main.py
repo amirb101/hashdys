fc279e1271ac75f8abb6771cb7da762a9d818133
LizziePReed <lizzie.preed@gmail.com>
2022-02-24 18:47:50 +0000

    a

--- main.py
+++ main.py
@@ -1,43 +1,25 @@
 c,p = map(int,input().split())
 contributers  = []
-<<<<<<< HEAD
 projects = []
-=======
-
-class person():
-  def __init__(self):
-    self.name = ""
-    self.skills = {}
-    
->>>>>>> 22180dace7cfac07bfe0f81c0f879b86b3c3f67f
 class project():
     def _init_(self):
         self.name = ""
         self.days = 0
         self.score = 0
         self.best = 0
+        self.roles=0
         self.jobs= {}
 
 
 for i in range(p):
     projects.append(project())
     current = list(input().split(" "))
-    project[i].name = current[0]
-    project[i].days = current[1]
-    project[i].score =current[2]
-    project[i].best = current[3]
+    projects[i].name = current[0]
+    projects[i].days = current[1]
+    projects[i].score =current[2]
+    projects[i].best = current[3]
+    projects[i].roles = current[4]
+    for j in range(current[4]):
+        job = list(input().split(" "))
+        project[i].jobs[job[0]] = int(job[1])
 
-
-
-
-
-<<<<<<< HEAD
-for i in range(c):
-    print("hi")
-class Person():
-  name = ""
-  skills = {}
-  
-=======
-    
->>>>>>> 22180dace7cfac07bfe0f81c0f879b86b3c3f67f
