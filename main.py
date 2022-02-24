a67e4f8dc21547ef4b65142a9fc1c03142279082
LizziePReed <lizzie.preed@gmail.com>
2022-02-24 18:39:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ contributers  = []
 class project():
     def _init_(self):
         self.name = ""
+        self.days = 0
+        self.score = 0
+        self.best = 0
         self.levels = {}
 
-for i in range(c):
+for i in range(p):
+    current = list(input().split(" "))
+    current
+
+
 
 
     
